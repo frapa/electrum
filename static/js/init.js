f78690b3ce483dca5314d0058e2f37d2b46bd5db
Davide Bazzanella <bazzanella.davide@gmail.com>
2016-12-11 18:27:54 +0100

    login screen

--- static/js/init.js
+++ static/js/init.js
@@ -2,4 +2,7 @@ $(function () {
     // Starts program
     Electrum.mainView = new App_View_Main();
     RootView.open(Electrum.mainView);
+    
+	var logInScreen = new App_View_LogIn();
+	Electrum.mainView.open(logInScreen, 'login-screen')
 });
---
+++ static/views/login.js
@@ -0,0 +1,23 @@
+var App_View_LogIn = AbstractView.extend({
+	subviews: {
+		name: new Kernel_View_Ui_Entry({
+			label: 'username'
+		}),
+		password: new Kernel_View_Ui_Entry({
+			label: 'password'
+		})
+	},
+	
+	events: {
+		'click #log-in-button': 'login'
+	},
+	
+	login: function() {
+		var username = this.subviews.name.getValue();
+		var password = this.subviews.password.getValue();
+		
+		if (!username && !password) {
+			this.remove();
+		}
+	},
+});
