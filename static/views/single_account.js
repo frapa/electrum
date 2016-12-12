bc79ba2dad88376306a9ea463cd47216a66057ef
Francesco Pasa <francescopasa@gmail.com>
2016-12-12 21:47:46 +0100

    row adding partially working

--- static/views/single_account.js
+++ static/views/single_account.js
@@ -2,10 +2,16 @@ var App_View_SingleAccount = AbstractView.extend({
     initialize: function (options) {
         Electrum.router.navigate('accounts/' + this.model.get('Id'));
 
+        var _this = this;
+
         this.subviews = {
             transactionTable: new Kernel_View_Ui_Table({
                 collection: this.model.to('In'),
                 inlineEditing: true,
+                addingRow: true,
+                addingRowBeforeSave: function (model) {
+                    model.link('To', _this.model);
+                },
                 columns: [
                     {header: 'Date', attr: 'Date'},
                     {header: 'Description', attr: 'Description'},
