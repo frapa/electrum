7d7a0a337a8236a97ee72606369df1d14dec2f54
Francesco Pasa <francescopasa@gmail.com>
2016-12-18 17:48:53 +0100

    fexed merge mess

--- static/views/accounts.js
+++ static/views/accounts.js
@@ -2,37 +2,6 @@ Electrum.accounts = new App_Collection_Account({url: '/controller/accounts/root'
 Electrum.allAccounts = new App_Collection_Account();
 
 var AccountsView = AbstractView.extend({
-<<<<<<< HEAD
-    subviews: {
-        'accountsTable': new Kernel_View_Ui_Tree({
-            collection: Electrum.accounts,
-            children: 'SubAccounts',
-            columns: [
-                {header: 'Name', attr: 'Name'},
-                {header: 'Description', attr: 'Description'},
-                {header: 'Total', method: 'computeTotal'}
-            ],
-            click: function (account) {
-                Electrum.mainView.open(
-                    new App_View_SingleAccount({model: account}),
-                    'main'
-                );
-            },
-            actions: [
-                {
-                    icon: 'icon-list-add',
-                    callback: function () {
-                        var dialog = new App_View_Dialog_NewAccount(
-                            this.model, this.childCollection);
-                        dialog.show(this.$button, 'ne', {tableAction: true});
-                    },
-                    tooltip: 'Add subaccount'
-                },
-                {
-                    icon: 'icon-trash',
-                    callback: function () {
-                        var model = this.model;
-=======
 	initialize: function (title, filterBy) {
 		var rootAccounts = new App_Collection_Account(
 			Electrum.accounts.where({Type: filterBy})
@@ -88,13 +57,13 @@ var AccountsView = AbstractView.extend({
 	},
 	
 	showAddSubaccountDialog: function () {
-		var dialog = new App_View_Dialog_NewAccount(this.model, this.childCollection);
+        var dialog = new App_View_Dialog_NewAccount(this.model,
+            this.childCollection);
 		dialog.show(this.$button, 'ne', {tableAction: true});
 	},
 	
 	showDeleteDialog: function () {
 		var model = this.model;
->>>>>>> 2c6a5cebbe65754e4f02b5c9f07ed24b66b42533
 
 		var dialog = Kernel_View_Ui_Dialog.extend({
 			title: 'Delete?',
